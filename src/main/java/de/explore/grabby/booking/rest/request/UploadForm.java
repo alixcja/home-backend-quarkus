1360aad0a04da6e398009ca2b132407442672f9b
alixcja <alicja.pater.2004@gmail.com>
2025-03-11 23:31:07 +0100

    🐛 Fix validation for uploadform

--- src/main/java/de/explore/grabby/booking/rest/request/UploadForm.java
+++ src/main/java/de/explore/grabby/booking/rest/request/UploadForm.java
@@ -1,6 +1,6 @@
 package de.explore.grabby.booking.rest.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.PartType;
 import org.jboss.resteasy.reactive.RestForm;
@@ -8,7 +8,7 @@ import org.jboss.resteasy.reactive.RestForm;
 import java.io.File;
 
 public class UploadForm {
-  @NotBlank(message = "File must be set")
+  @NotNull(message = "File must be set")
   @RestForm("file")
   @PartType(MediaType.APPLICATION_OCTET_STREAM)
   public File file;
